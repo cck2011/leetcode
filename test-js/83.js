ff6d01d727047e23c0acf8c15f172ee733e5ef21
Calvin Chan <calvin199867@gmail.com>
2022-03-22 15:16:26 +0800

    hi

--- test-js/83.js
+++ test-js/83.js
@@ -31,3 +31,17 @@ function deleteDuplicates(head) {
     head.next = deleteDuplicates(head.next);
     return head.val == head.next.val ? head.next : head;
 }
+
+function deleteDuplicates(head) {
+     
+        if (head == null || head.next == null) return head;
+        // 先指向下一個,assume no duplicates
+        head.next = deleteDuplicates(head.next);
+        //若当前节点的值与其后驱节点是重复的，删除后驱节点
+        
+        if(head.val == head.next.val) head.next = head.next.next;
+        //返回当前节点
+        //
+        return head;
+        
+    }
